3a430fbaf5d956f00e1e8c35f067086d9bdebe90
jos <jos.onokiewicz@han.nl>
2019-07-21 18:36:23 +0200

    More modern C++

--- app/math/XYZrZ.cpp
+++ app/math/XYZrZ.cpp
@@ -1,67 +1,68 @@
 #include "XYZrZ.h"
-#include "math/MathDef.h"
+#include "MathDef.h"
 
 std::ostream &operator<<(std::ostream &os, const XYZrZ &rhs)
 {
    os << "[" << rhs.position << ", " << rhs.Rz << "]";
+
    return os;
 }
 
 XYZrZ operator+(const XYZrZ &lhs, const XYZrZ &rhs)
 {
-   return XYZrZ(lhs.position + rhs.position, lhs.Rz + rhs.Rz);
+   return {lhs.position + rhs.position, lhs.Rz + rhs.Rz};
 }
 
 XYZrZ operator-(const XYZrZ &lhs, const XYZrZ &rhs)
 {
-   return XYZrZ(lhs.position - rhs.position, lhs.Rz - rhs.Rz);
+   return {lhs.position - rhs.position, lhs.Rz - rhs.Rz};
 }
 
 XYZrZ operator*(const XYZrZ &lhs, double rhs)
 {
-   return XYZrZ(lhs.position * rhs);
+   return {lhs.position * rhs};
 }
 
 XYZrZ operator+(const XYZrZ &lhs, const CartVec &rhs)
 {
-   return XYZrZ(lhs.position + rhs, lhs.Rz);
+   return {lhs.position + rhs, lhs.Rz};
 }
 
 XYZrZ operator-(const XYZrZ &lhs, const CartVec &rhs)
 {
-   return XYZrZ(lhs.position - rhs, lhs.Rz);
+   return {lhs.position - rhs, lhs.Rz};
 }
 
 XYZrZ operator+(const XYZrZ &lhs, double rhs)
 {
-   return XYZrZ(lhs.position, math::normalizeDegrees(lhs.Rz + rhs));
+   return {lhs.position, math::normalizeDegrees(lhs.Rz + rhs)};
 }
 
 XYZrZ operator-(const XYZrZ &lhs, double rhs)
 {
-   return XYZrZ(lhs.position, math::normalizeDegrees(lhs.Rz - rhs));
+   return {lhs.position, math::normalizeDegrees(lhs.Rz - rhs)};
 }
 
 XYZrZ::XYZrZ(double x, double y, double z, double rz)
-   : position(x, y, z)
-   , Rz(rz)
+   : position{x, y, z}
+   , Rz{rz}
 {
 }
 
 XYZrZ::XYZrZ(const CartVec &xyz)
-   : position(xyz)
-   , Rz(0)
+   : position{xyz}
+   , Rz{0.0}
 {
 }
 
 XYZrZ::XYZrZ(const CartVec &xyz, double rz)
-   : position(xyz)
-   , Rz(rz)
+   : position{xyz}
+   , Rz{rz}
 {
 }
 
 void XYZrZ::draw() const
 {
    glTranslatef(position.get_x(), position.get_y(), position.get_z());
-   glRotatef(Rz, 0, 0, 1);
+   glRotatef(Rz, 0.0f, 0.0f, 1.0f);
 }
