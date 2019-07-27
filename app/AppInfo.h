81beb61b9f11953ce94a75cd94ec418c0fea8b0e
jos <jos.onokiewicz@han.nl>
2019-07-27 17:59:28 +0200

    Updated version

--- app/AppInfo.h
+++ app/AppInfo.h
@@ -4,9 +4,9 @@
 #define APPNAME "YAAVsimulator development"
 
 #define MAJOR_VERSION "1"
-#define MINOR_VERSION "0"
-#define REVISION_VERSION "12"
+#define MINOR_VERSION "1"
+#define REVISION_VERSION "0"
 #define VERSION MAJOR_VERSION "." MINOR_VERSION "." REVISION_VERSION
-#define APPNAME_VERSION APPNAME " v" VERSION " development"
+#define APPNAME_VERSION APPNAME " v" VERSION
 
 #endif
