3f12087a60084d6100ecc2141f2655a62d8452ab
jos <jos.onokiewicz@han.nl>
2019-07-27 16:52:34 +0200

    Solved bug, more modern C++

--- app/drawables/Room.cpp
+++ app/drawables/Room.cpp
@@ -4,8 +4,8 @@
 #include <sstream>
 
 Room::Room()
-   : corners_{{-3, -2, 0}, {-3, 1, 0}, {0, 1, 0},  {0, 2, 0},
-               {2, 2, 0},   {2, -2, 0}, {-3, -2, 0}}
+   : corners_{{-3, -2, 0}, {-3, 1, 0}, {0, 1, 0},
+              {0, 2, 0},   {2, 2, 0},  {2, -2, 0}}
 {
    SET_FNAME("Room::Room()");
    corners_.calcNormal();
@@ -18,26 +18,22 @@ void Room::draw() const
 {
    glPushMatrix();
    glColor4f(0.6f, 0.2f, 0.2f, 1.0f); // line color
-   double lw = 0;
+   double lw = 0.0;
    glGetDoublev(GL_LINE_WIDTH, &lw);
-   glLineWidth(lw * 1.3);
+   glLineWidth(lw * 1.4);
    glBegin(GL_POLYGON);
-   for (size_t i = 0; i < corners_.getVertices().size(); ++i) {
-      glVertex3f(corners_[i].get_x(), corners_[i].get_y(),
-                 corners_[i].get_z());
+   for (const auto &corner : corners_.getVertices()) {
+      glVertex3f(corner.get_x(), corner.get_y(), corner.get_z());
    }
    glVertex3f(corners_[0].get_x(), corners_[0].get_y(), corners_[0].get_z());
    glEnd();
    glColor4f(0.15f, 0.15f, 0.15f, 0.7f); // line color
    glLineWidth(lw);
    glBegin(GL_LINES);
-   for (size_t i = 0; i < corners_.getVertices().size(); ++i) {
-      glVertex3f(corners_[i].get_x(), corners_[i].get_y(),
-                 corners_[i].get_z());
-      glVertex3f(corners_[i].get_x(), corners_[i].get_y(),
-                 corners_[i].get_z() + 0.5);
+   for (const auto &corner : corners_.getVertices()) {
+      glVertex3f(corner.get_x(), corner.get_y(), corner.get_z());
+      glVertex3f(corner.get_x(), corner.get_y(), corner.get_z() + 0.5);
    }
-   // glVertex3f(m_corners[0].x, m_corners[0].y, m_corners[0].z);
    glEnd();
    glLineWidth(lw);
    glPopMatrix();
